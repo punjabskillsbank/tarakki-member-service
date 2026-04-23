d894b84a9ab714e803fe12e32639a786717b0adc
codebyjsa <jsaishere@gmail.com>
2026-04-23 11:43:58 +0530

    TK_19: Implemented CORS configuration from common repo

--- src/main/java/com/tarakki/member/TarakkiMemberServiceApplication.java
+++ src/main/java/com/tarakki/member/TarakkiMemberServiceApplication.java
@@ -1,12 +1,15 @@
 package com.tarakki.member;
 
+import com.tarakki.common.config.CorsConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.context.annotation.Import;
 
 
 @SpringBootApplication
 @EntityScan(basePackages = "com.tarakki.common.entity")
+@Import(CorsConfig.class)
 public class TarakkiMemberServiceApplication {
 
 	public static void main(String[] args) {
