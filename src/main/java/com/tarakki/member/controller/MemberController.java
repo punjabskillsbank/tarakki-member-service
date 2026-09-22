e4fa5d53358799977bb9be5bd29409ac0ec8f2a7
Gurwinder-dotcom <gurwinderofficial9@gmail.com>
2026-09-22 21:54:32 +0530

    TK_142: Create Audit Log Logic for DELETE API in Member Controller

--- src/main/java/com/tarakki/member/controller/MemberController.java
+++ src/main/java/com/tarakki/member/controller/MemberController.java
@@ -1,8 +1,10 @@
 package com.tarakki.member.controller;
 
+import com.tarakki.common.audit.annotation.Auditable;
 import com.tarakki.common.dto.MemberDTO;
 import com.tarakki.member.dto.MemberRequestDTO;
 import com.tarakki.member.dto.MemberUpdateDTO;
+import com.tarakki.member.entity.Member;
 import com.tarakki.member.service.MemberService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -46,6 +48,7 @@ public class MemberController {
         return ResponseEntity.ok(members);
     }
 
+    @Auditable(eventName = "MEMBER_DELETED" , entityName = "MEMBER" , entityClass = Member.class , entityIdArgSpel = "#memberId")
     @DeleteMapping("/{memberId}")
     public ResponseEntity<String> deleteMember(@PathVariable UUID memberId) {
         memberService.deleteMember(memberId);
